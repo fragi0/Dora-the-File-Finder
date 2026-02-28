3caeb0f6439a5f131bdea897c5cd12153100db84
UxueLo <uxueloizaga@gmail.com>
2026-02-28 05:56:04 +0100

    SearchBar & Button

--- src/gui/app/page.tsx
+++ src/gui/app/page.tsx
@@ -3,8 +3,9 @@ import SearchBar from "@/components/SearchBar";
 
 export default function Home() {
   return (
-    <main>
+    <div className="flex items-center justify-center min-h-screen bg-gray-700">
       <SearchBar />
-    </main>
+      <button className= "rounded-3xl bg-gray-800 text-white px-6 py-2 hover:bg-blue-400 hover:text-gray-900 transition h-14 ml-6">Upload </button>
+    </div>
   );
 }
