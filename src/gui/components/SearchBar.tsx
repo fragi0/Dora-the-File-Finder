b1297814c5df0da30f41f08fae73b499badb24ef
fragi0 <ssejico@gmail.com>
2026-02-28 15:16:51 +0100

    Added personalization to Upload button

--- src/gui/components/SearchBar.tsx
+++ src/gui/components/SearchBar.tsx
@@ -16,7 +16,7 @@ export default function SearchBar(){
 
     return(
         <div className="flex">
-            <div className="flex border border-gray-700 rounded-3xl px-2 py-2 mb-2 w-250 h-14 bg-white/35 shadow-2xl ">
+            <div className="flex border border-gray-700 rounded-3xl px-2 py-2 mb-2 mt-52 w-250 h-14 bg-white/35 shadow-2xl ">
             <input 
             className="text-center w-220 focus:outline-none text-gray-700 mr-3.5" 
             type = "text" 
@@ -25,7 +25,7 @@ export default function SearchBar(){
             onChange={(e) => setRequest(e.target.value)}
             />
             </div> 
-            <button onClick={searchAction} className="rounded-3xl bg-gray-800 text-white px-6 py-2 hover:bg-blue-400 hover:text-gray-900 transition h-14 ml-6">Search</button>
+            <button onClick={searchAction} className="rounded-3xl bg-gray-800 text-white px-6 py-2 mt-52 hover:bg-blue-400 hover:text-gray-900 transition h-14 ml-6 hover:cursor-pointer">Search</button>
         </div>
          
 
