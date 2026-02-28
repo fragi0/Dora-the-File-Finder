460225f54f060777bea146ca7bb7cdba0581818f
fragi0 <ssejico@gmail.com>
2026-02-28 07:11:38 +0100

    Finished background personalization

--- src/gui/components/SearchBar.tsx
+++ src/gui/components/SearchBar.tsx
@@ -2,7 +2,7 @@
 
 export default function SearchBar(){
     return(
-        <div className="flex border border-gray-700 rounded-3xl px-2 py-2 mb-2 w-224 h-14 bg-white/20 shadow-2xl ">
+        <div className="flex border border-gray-700 rounded-3xl px-2 py-2 mb-2 w-4xl h-14 bg-white/35 shadow-2xl ">
             <input className="text-center w-220 focus:outline-none text-gray-700 mr-3.5" type = "text" placeholder = "Search a file..." />
         </div>  
     )
