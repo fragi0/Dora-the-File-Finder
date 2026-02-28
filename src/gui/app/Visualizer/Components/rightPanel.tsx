05fe9b11ada276af23894eea1db5205756cd1fb7
UxueLo <uxueloizaga@gmail.com>
2026-02-28 19:49:09 +0100

    solution to data.map problem

--- src/gui/app/Visualizer/Components/rightPanel.tsx
+++ src/gui/app/Visualizer/Components/rightPanel.tsx
@@ -2,23 +2,33 @@
 
 export default function RightPanel({ selectedFile }: { selectedFile: any }) {
     if (!selectedFile) {
-        return <div style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center', color: '#64748b' }}>
-            Selecciona un archivo para visualizarlo
-        </div>;
+        return ;
     }
 
     return (
-        <div style={{ flex: 1, padding: '20px', display: 'flex', flexDirection: 'column' }}>
-            <h1 style={{ color: '#38bdf8', marginBottom: '20px' }}>
-                {selectedFile.original_filename}
-            </h1>
-            
-            <div style={{ flex: 1, backgroundColor: 'white', borderRadius: '12px', overflow: 'hidden' }}>
-                <iframe 
-                    src={`http://localhost:8000/files/${selectedFile.original_filename}`}
-                    style={{ width: '100%', height: '100%', border: 'none' }}
-                />
+        
+        <div className="flex flex-col h-screen w-full">
+            <div className="h-1/2 border-b border-slate-700 p-6 overflow-y-auto bg-gray-600">
+                <h1 className="text-white" style={{ marginBottom: '20px' }}>
+                    {selectedFile.original_filename}
+                </h1>
             </div>
+                
+            <div className="h-1/2 p-6 overflow-y-auto bg-gray-800">
+                <h1 className="text-xl font-bold text-white">  
+                    {selectedFile.original_filename}
+                </h1>
+            </div>
+    
         </div>
     );
+
+    function pdfDisplay(){
+    <iframe
+                   src={`http://localhost:8000/${selectedFile.path}`}
+                   style={{ width: '100%', height: '100%', border: 'none' }}
+                />
 }
+
+}
+
