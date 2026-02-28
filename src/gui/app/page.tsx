b06040fa55cb44f4c4a598656e42a4676cc9e6df
UxueLo <uxueloizaga@gmail.com>
2026-02-28 06:44:03 +0100

    Broken BackGround

--- src/gui/app/page.tsx
+++ src/gui/app/page.tsx
@@ -3,9 +3,12 @@ import SearchBar from "@/components/SearchBar";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-700">
-      <SearchBar />
-      <button className= "rounded-3xl bg-gray-800 text-white px-6 py-2 hover:bg-blue-400 hover:text-gray-900 transition h-14 ml-6">Upload </button>
+    <div className="relative w-full h-screen bg-cover bg-center" style={{backgroundImage: "url('./public/imagenFondoDora.p')"}}>
+       <div className="flex items-center justify-center min-h-screen">
+        <SearchBar />
+        <button className= "rounded-3xl bg-gray-800 text-white px-6 py-2 hover:bg-blue-400 hover:text-gray-900 transition h-14 ml-6">Upload </button>
+      </div>
     </div>
+   
   );
 }
--- src/gui/components/SearchBar.tsx
+++ src/gui/components/SearchBar.tsx
@@ -4,6 +4,6 @@ export default function SearchBar(){
     return(
         <div className="flex border border-gray-700 rounded-3xl px-2 py-2 mb-2 w-224 h-14 bg-white/20 shadow-2xl ">
             <input className="text-center w-220 focus:outline-none text-gray-700 mr-3.5" type = "text" placeholder = "Search a file..." />
-        </div>
+        </div>  
     )
 }
